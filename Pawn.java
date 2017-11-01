91e9b41f93f5d45c451cfdafd7326ffe52e41f39
Amani Konduru <akonduru2@student.gsu.edu>
2017-11-01 00:22:42 -0400

    Update Pawn.java
    just took out the excess comments

--- Pawn.java
+++ Pawn.java
@@ -1,30 +1,24 @@
 //import java.util.Arrays;
 /**
  * Represents a Piece named Bishop which overrides Piece’s abstract methods
- *
  * @author akonduru2
  * @version 1.0
  */
 public class Pawn extends Piece {
-
 /**
-*
 *  @param clr the color of the piece
 */
     public Pawn(Color clr) {
         super(clr);
     }
-
     @Override
     public String algebraicName() {
         return "";
     }
-
     @Override
     public String fenName() {
         //color
         Color clr = this.getColor();
-
         if (clr == Color.WHITE) {
             return "P";
         } else {
@@ -50,8 +44,7 @@ public class Pawn extends Piece {
         } else {
             movesArr = new Square[1];
         }
-
-        //white
+        //white pawn
         if (clr == Color.WHITE) {
             if (rank == '2') {
                 //System.out.println("row 2 whitep 2 moves in array");
@@ -62,10 +55,6 @@ public class Pawn extends Piece {
                 movesArr[1] = new Square(file , (char) (rank + 2));
                 //System.out.println("blah");
             } else if (rank == '8') {
-                //System.out.println("scozcnm");
-                //System.out.println(file);
-                //System.out.println(rank);
-                //System.out.println(Arrays.toString(new Square[0]));
                 //movesArr[0] = new Square((char) 0, (char) 0);
                 movesArr = new Square[0];
                 //System.out.println(Arrays.toString(movesArr));
@@ -77,7 +66,7 @@ public class Pawn extends Piece {
                 //System.out.println(Arrays.toString(movesArr));
             }
         }
-        //black
+        //black pawn
         if (clr == Color.BLACK && rank != '8') {
             if (rank == '7') {
                 //System.out.println("row 7 blackp 2 moves in array");
@@ -88,38 +77,12 @@ public class Pawn extends Piece {
                 movesArr[1] = new Square(file , (char) (rank - 2));
                 //System.out.println(Arrays.toString(movesArr));
             } else if (rank == '1') {
-                //System.out.println(file);
-                //System.out.println(rank);
-                //System.out.println(Arrays.toString(movesArr));
                 movesArr[0] = new Square('\0' , '\0');
                 //System.out.println(Arrays.toString(movesArr));
             } else { //remove
-                //System.out.println("blackp 1 move in array");
-                //System.out.println(file);
-                //System.out.println(rank);
-                //System.out.println(Arrays.toString(movesArr));
                 movesArr[0] = new Square(file , (char) (rank - 1));
-                //System.out.println(file);
-                //System.out.println(rank);
-                //System.out.println(Arrays.toString(movesArr));
             }
         }
-        // int count = 0;
-        // if(movesArr.length == 0) {
-        //     count++;
-        // } else if (movesArr.length == 1) {
-        //     count++;
-        // } else {
-        //     for (int i = 0; i < 2 ; i++) {
-        //         if (movesArr[i] != null) {
-        //         count++;
-        //         }
-        //     }
-        // }
-        // Square[] pmovesArr = new Square[count];
-        // for (int i = 0; i < count; i++) {
-        //     pmovesArr[i] = movesArr[i];
-        // }
         return movesArr;
     }
 }
